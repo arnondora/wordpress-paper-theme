b6191892cddeb19b2f7bfc2c94c5b785037e6444
Arnon Puitrakul <peter_arnon1@icloud.com>
2015-06-25 22:47:11 +0700

    Add Search form for mobile devices

--- PaperTheme/index.php
+++ PaperTheme/index.php
@@ -2,6 +2,7 @@
 <head><title><?php bloginfo('name'); echo ' - '; bloginfo('description');?></title></head>
 	<div class="container">
 		<div class="col-md-9">
+			<div class = "hidden-lg hidden-md"> <?php get_search_form(); ?> </div>
 			<?php if (have_posts()) : while(have_posts()) : the_post();?>
 			<div class="row shadow-box">
 					<a href="<?php the_permalink();?>" title = "<?php the_title();?>"><h3 class = "text-center"><?php the_title();?></h3></a>
