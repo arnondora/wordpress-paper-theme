e4ed4cd72a5ef1e2c4f6bdea2a8e2fbc12a7f647
Arnon Puitrakul <peter_arnon1@icloud.com>
2015-06-25 23:08:40 +0700

    Add Search.php for showing search result

---
+++ PaperTheme/search.php
@@ -0,0 +1,23 @@
+<?php get_header();?>
+
+<?php $catid = get_cat_id( single_cat_title("",false) );?>
+
+<head><title>Search result for <?php   echo '"' . $_GET['s'] . '"' . ' - '; bloginfo('name'); ?></title></head>
+<div class="container">
+
+	<?php get_search_form();?>
+
+	<?php if (have_posts()) : while(have_posts()) : the_post();?>
+		<div class="shadow-box row">
+			<a href="<?php the_permalink();?>" title = "<?php the_title();?>"><h3 class = "text-center"><?php the_title();?></h3></a>
+			<h6 class = "text-muted text-center">Posted by <?php the_author();?> on <?php the_time('F jS, Y'); ?></h6>
+
+			<center><?php if (has_post_thumbnail()) {the_post_thumbnail('large',array('class' => 'img-responsive img-thumbnail'));} ?></center>
+			<p class = "content"><?php the_excerpt(); ?></p>
+		</div>
+		<?php endwhile; else : ?>
+				<p><?php _e('No post right now.'); ?></p>
+		<?php endif; ?>
+</div>
+
+<?php get_footer();?>
