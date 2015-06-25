d162281ba548b0ccebdf5169458172a238ec2965
Arnon Puitrakul <peter_arnon1@icloud.com>
2015-06-25 23:38:56 +0700

    Fix linking bootstrap.min.js

--- PaperTheme/footer.php
+++ PaperTheme/footer.php
@@ -10,6 +10,6 @@
 </body>
 
 <script src = "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src = "<?php get_template_directory(); ?>/js/bootstrap.js"></script>
+<script src = "<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
 
 </html>
