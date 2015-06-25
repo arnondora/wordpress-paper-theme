a784527ac957c57a74a83452be146a1ad93ecad7
Arnon Puitrakul <peter_arnon1@icloud.com>
2015-06-25 18:03:44 +0700

    Add new page.php

--- PaperTheme/single.php
+++ PaperTheme/single.php
@@ -3,13 +3,40 @@
 <!-- Condition if have post -->
 <?php if(have_posts()) : while (have_posts()) : the_post();?>
 	<div class="container">
+		<!-- Main Content -->
 		<div class = "shadow-box" style = "padding-top:5px;">
 			<a href="<?php the_permalink(); ?>"><h3 class= "text-center"><?php the_title(); ?></h3></a>
+			<center> <p class = "text-muted">Posted by <?php the_author();?> on <?php the_time('F jS, Y'); ?></p> </center>
 			<?php if (has_post_thumbnail()) : ?>
 				<div class="row" style = "margin-top:10px;"><center><?php the_post_thumbnail('large',array('class' => 'full-width img-responsive img-thumbnail')); ?></center></div>
 			<?php endif; ?>
 			<div class="row"><p><?php the_content(); ?></p></div>
 		</div>
+		
+		<!-- Tag and Category -->
+		<div class = "shadow-box" style="padding-top:5px;" >
+			<div style = "margin-top:5px;">
+				<span>Categories & Tags : </span>
+				<?php
+					$categories = get_the_category();
+					foreach ($categories as $category)
+					{
+						echo '<a href="' . get_category_link($category->term_id).'" class="btn btn-default" style = "margin-left : 4px;">' . $category->cat_name. '</a>';
+					}
+
+					if (has_tag()) : 
+					$tags = get_the_tags();
+					foreach ($tags as $tag)
+					{
+						echo '<a href="' . get_tag_link($tag->term_id). '" class = "btn btn-info" style = "margin-left : 4px;">' . $tag->name. '</a>';
+					}
+					endif;
+				?>
+			</div>
+		</div>
+
+		<!-- Comment -->
+		<div class="shadow-box" style = "padding-top:5px;"><?php comments_template(); ?></div>
 	</div>
 <?php endwhile; else: ?>
 <?php endif;?>
