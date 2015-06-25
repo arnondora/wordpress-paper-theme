2746983cb2b47898b2d932b5d2bcf9e315baf92b
Arnon Puitrakul <peter_arnon1@icloud.com>
2015-06-25 19:28:09 +0700

    Add new category.php

---
+++ PaperTheme/category.php
@@ -0,0 +1,23 @@
+<?php get_header();?>
+
+<?php $catid = get_cat_id( single_cat_title("",false) );?>
+<div class="container">
+	<div class="shadow-box" style = "padding-top : 5px;">
+		<h5>Category :  <?php single_cat_title('',true); ?></h5>
+		<p> <?php echo category_description($catid); ?> </p>
+	</div>
+
+	<?php if (have_posts()) : while(have_posts()) : the_post();?>
+		<div class="shadow-box">
+			<a href="<?php the_permalink();?>" title = "<?php the_title();?>"><h3 class = "text-center"><?php the_title();?></h3></a>
+			<h6 class = "text-muted text-center">Posted by <?php the_author();?> on <?php the_time('F jS, Y'); ?></h6>
+
+			<center><?php if (has_post_thumbnail()) {the_post_thumbnail('large',array('class' => 'img-responsive img-thumbnail'));} ?></center>
+			<p class = "content"><?php the_excerpt(); ?></p>
+		</div>
+		<?php endwhile; else : ?>
+				<p><?php _e('No post right now.'); ?></p>
+		<?php endif; ?>
+</div>
+
+<?php get_footer();?>
