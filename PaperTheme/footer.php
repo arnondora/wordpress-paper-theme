4df13a62479c45fec5cebe7c00757085d88464aa
Arnon Puitrakul <peter_arnon1@icloud.com>
2015-06-26 12:42:00 +0700

    Remove inline style and move to #footer-text

--- PaperTheme/footer.php
+++ PaperTheme/footer.php
@@ -1,13 +1,12 @@
 	<div class = "footer navbar navbar-default" style="margin-bottom:0px;">
 		<div class="container">
-			<p class = "navbar-text pull-left" style = "color:#2196f3;">Copyright 2014-2015 Arnon Puitrakul all right reversed.</p>
-			<p class="navbar-text pull-right" style = "color :#2196f3;">Paper Theme by <a href="http://www.arnondora.in.th">@arnondora</a>
+			<p class = "navbar-text pull-left" id = "footer-text">Copyright 2014-2015 Arnon Puitrakul all right reversed.</p>
+			<p class="navbar-text pull-right" id = "footer-text">Paper Theme by <a href="http://www.arnondora.in.th">@arnondora</a>
 		</div>
 	</div>
 <?php wp_footer();?>
 </body>
 
-<script src = "<?php echo get_template_directory_uri(); ?>/js/jquery.min.js"></script>
-<script async src = "<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
+<script async src = "<?php echo get_template_directory_uri(); ?>/js/all.min.js"></script>
 
 </html>
