596ff63e722c79a5e3b92656134145aab53c4470
Arnon Puitrakul <peter_arnon1@icloud.com>
2015-06-27 15:24:29 +0700

    Add meta widget styling

--- PaperTheme/functions.php
+++ PaperTheme/functions.php
@@ -477,6 +477,61 @@
 
 	}
 
+	//change meta widget style
+	class My_Widget_Meta extends WP_Widget {
+
+	public function __construct() {
+		$widget_ops = array('classname' => 'widget_meta', 'description' => __( "Login, RSS, &amp; WordPress.org links.") );
+		parent::__construct('meta', __('Meta'), $widget_ops);
+	}
+
+	public function widget( $args, $instance ) {
+
+		/** This filter is documented in wp-includes/default-widgets.php */
+		$title = '<h4>'. apply_filters( 'widget_title', empty($instance['title']) ? __( 'Meta' ) : $instance['title'], $instance, $this->id_base ) . '</h4>';
+
+		echo $args['before_widget'];
+		if ( $title ) {
+			echo $args['before_title'] . $title . $args['after_title'];
+		}
+		?>
+			<div class="shadow-box">
+				<ul class = "nav nav-default">
+				<?php wp_register(); ?>
+				<li><?php wp_loginout(); ?></li>
+				<li><a href="<?php bloginfo('rss2_url'); ?>"><?php _e('Entries <abbr title="Really Simple Syndication">RSS</abbr>'); ?></a></li>
+				<li><a href="<?php bloginfo('comments_rss2_url'); ?>"><?php _e('Comments <abbr title="Really Simple Syndication">RSS</abbr>'); ?></a></li>
+			<?php
+				echo apply_filters( 'widget_meta_poweredby', sprintf( '<li><a href="%s" title="%s">%s</a></li>',
+					esc_url( __( 'https://wordpress.org/' ) ),
+					esc_attr__( 'Powered by WordPress, state-of-the-art semantic personal publishing platform.' ),
+					_x( 'WordPress.org', 'meta widget link text' )
+				) );
+
+				wp_meta();
+		?>
+				</ul>
+			</div>
+	<?php
+		echo $args['after_widget'];
+	}
+
+	public function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+
+		return $instance;
+	}
+
+	public function form( $instance ) {
+		$instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
+		$title = strip_tags($instance['title']);
+?>
+			<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
+<?php
+	}
+}
+
 
 
 	//Register custom widget
@@ -497,6 +552,11 @@
 		//Register Page Widget
 		unregister_widget('WP_Widget_Pages');
 		register_widget('My_Widget_Pages');
+
+		//Register Meta Widget
+		unregister_widget('WP_Widget_Meta');
+		register_widget('My_Widget_Meta');
+
 	}
 	add_action('widgets_init', 'Register_My_Widget');
 
