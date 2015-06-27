4dfa12046949fded1c97c79e948b9ac7d480e417
Arnon Puitrakul <peter_arnon1@icloud.com>
2015-06-27 15:07:10 +0700

    Add page widget styling

--- PaperTheme/functions.php
+++ PaperTheme/functions.php
@@ -162,11 +162,6 @@
 	}
 
 	//change archives widget style
-		/**
-	 * Archives widget class
-	 *
-	 * @since 2.8.0
-	 */
 	class My_Widget_Archives extends WP_Widget {
 
 		public function __construct() {
@@ -286,11 +281,6 @@
 	}
 
 	//change categories widget style
-		/**
-		 * Categories widget class
-		 *
-		 * @since 2.8.0
-		 */
 		class My_Widget_Categories extends WP_Widget {
 
 			public function __construct() {
@@ -364,14 +354,6 @@
 					<ul class="list-unstyled nav nav-pills nav-stacked">
 			<?php
 					$cat_args['title_li'] = '';
-
-					/**
-					 * Filter the arguments for the Categories widget.
-					 *
-					 * @since 2.8.0
-					 *
-					 * @param array $cat_args An array of Categories widget options.
-					 */
 					wp_list_categories( apply_filters( 'widget_categories_args', $cat_args ) );
 			?>
 					</ul>
@@ -416,6 +398,85 @@
 
 		}
 
+	//change page widget style
+	class My_Widget_Pages extends WP_Widget {
+
+	public function __construct() {
+		$widget_ops = array('classname' => 'widget_pages', 'description' => __( 'A list of your site&#8217;s Pages.') );
+		parent::__construct('pages', __('Pages'), $widget_ops);
+	}
+
+	public function widget( $args, $instance ) {
+		$title = '<h4>' . apply_filters( 'widget_title', empty( $instance['title'] ) ? __( 'Pages' ) : $instance['title'], $instance, $this->id_base ) . '</h4>';
+
+		$sortby = empty( $instance['sortby'] ) ? 'menu_order' : $instance['sortby'];
+		$exclude = empty( $instance['exclude'] ) ? '' : $instance['exclude'];
+
+		if ( $sortby == 'menu_order' )
+			$sortby = 'menu_order, post_title';
+		$out = wp_list_pages( apply_filters( 'widget_pages_args', array(
+			'title_li'    => '',
+			'echo'        => 0,
+			'sort_column' => $sortby,
+			'exclude'     => $exclude
+		) ) );
+
+		if ( ! empty( $out ) ) {
+			echo $args['before_widget'];
+			if ( $title ) {
+				echo $args['before_title'] . $title . $args['after_title'];
+			}
+		?>
+		
+		<div class="shadow-box">	
+			<ul class = "nav nav-default">
+				<?php echo $out; ?>
+			</ul>
+		</div>
+		<?php
+			echo $args['after_widget'];
+		}
+	}
+
+	public function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		if ( in_array( $new_instance['sortby'], array( 'post_title', 'menu_order', 'ID' ) ) ) {
+			$instance['sortby'] = $new_instance['sortby'];
+		} else {
+			$instance['sortby'] = 'menu_order';
+		}
+
+		$instance['exclude'] = strip_tags( $new_instance['exclude'] );
+
+		return $instance;
+	}
+
+	public function form( $instance ) {
+		//Defaults
+		$instance = wp_parse_args( (array) $instance, array( 'sortby' => 'post_title', 'title' => '', 'exclude' => '') );
+		$title = esc_attr( $instance['title'] );
+		$exclude = esc_attr( $instance['exclude'] );
+	?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p>
+		<p>
+			<label for="<?php echo $this->get_field_id('sortby'); ?>"><?php _e( 'Sort by:' ); ?></label>
+			<select name="<?php echo $this->get_field_name('sortby'); ?>" id="<?php echo $this->get_field_id('sortby'); ?>" class="widefat">
+				<option value="post_title"<?php selected( $instance['sortby'], 'post_title' ); ?>><?php _e('Page title'); ?></option>
+				<option value="menu_order"<?php selected( $instance['sortby'], 'menu_order' ); ?>><?php _e('Page order'); ?></option>
+				<option value="ID"<?php selected( $instance['sortby'], 'ID' ); ?>><?php _e( 'Page ID' ); ?></option>
+			</select>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('exclude'); ?>"><?php _e( 'Exclude:' ); ?></label> <input type="text" value="<?php echo $exclude; ?>" name="<?php echo $this->get_field_name('exclude'); ?>" id="<?php echo $this->get_field_id('exclude'); ?>" class="widefat" />
+			<br />
+			<small><?php _e( 'Page IDs, separated by commas.' ); ?></small>
+		</p>
+			<?php
+		}
+
+	}
+
 
 
 	//Register custom widget
@@ -432,6 +493,10 @@
 		//Register Category Widget
 		unregister_widget('WD_Widget_Categories');
 		register_widget('My_Widget_Categories');
+
+		//Register Page Widget
+		unregister_widget('WP_Widget_Pages');
+		register_widget('My_Widget_Pages');
 	}
 	add_action('widgets_init', 'Register_My_Widget');
 
