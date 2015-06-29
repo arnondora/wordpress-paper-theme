8a376487ef69222246ddd5783337f96c4a3ead6c
Arnon Puitrakul <peter_arnon1@icloud.com>
2015-06-29 18:45:27 +0700

    Wrong spelling

--- PaperTheme/footer.php
+++ PaperTheme/footer.php
@@ -1,6 +1,6 @@
 	<div class = "footer navbar navbar-default" style="margin-bottom:0px;">
 		<div class="container">
-			<p class = "navbar-text pull-left" id = "footer-text">Copyright 2014-2015 Arnon Puitrakul all right reversed.</p>
+			<p class = "navbar-text pull-left" id = "footer-text">Copyright 2014-2015 Arnon Puitrakul all right reserved.</p>
 			<p class="navbar-text pull-right" id = "footer-text">Paper Theme by <a href="http://www.arnondora.in.th">@arnondora</a>
 		</div>
 	</div>
