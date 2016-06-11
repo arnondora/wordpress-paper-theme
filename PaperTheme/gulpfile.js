34d5fe17f293125fad131fdb140af41c88a8fbd6
Arnon Puitrakul <peter_arnon1@icloud.com>
2016-06-12 00:30:21 +0700

    Insert new function to compile sass and concat all css file together

---
+++ PaperTheme/gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+
+
+
+gulp.task('default',['sass'], function() {
+});
+
+gulp.task("sass", function () {
+    gulp.src("./sass/*.scss")
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(sourcemaps.write("./css"))
+    .pipe(gulp.dest("./css"));
+});
+
+gulp.task('concat', function() {
+  return gulp.src(['./sass/*.scss', './bower_components/bootswatch/bootstrap.min.css','./bower_components/font-awesome/css/font-awesome.min.css'])
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./sass/*.scss', ['concat']);
+  // Other watchers
+});
