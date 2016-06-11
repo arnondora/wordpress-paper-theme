f256d15af479d6c4d5923358ec4d3b49994fc189
Arnon Puitrakul <peter_arnon1@icloud.com>
2016-06-12 00:54:36 +0700

    Change main stylesheet path to be the new one

--- PaperTheme/header.php
+++ PaperTheme/header.php
@@ -6,7 +6,7 @@
 	<meta name = "viewport" content="width-device-width, initial-scale=1">
 	<meta name = "theme-color" content = "#9E9E9E"> <!-- Add theme-color for chrome >39 -->
 	<!-- <link href = "<?php bloginfo('stylesheet_url');?>" rel = "stylesheet"> -->
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.min.css" type="text/css" media="all" />
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style/style.css" type="text/css" media="all" />
 	<!-- <title><?php bloginfo('name'); ?></title> -->
 	<?php wp_head(); ?>
 	<style>
@@ -17,9 +17,9 @@
 
 		.shadow-box-colour
 		{
-			background-color: <?php echo of_get_option( 'paper_colour', '#FAFAFA');?>;	
+			background-color: <?php echo of_get_option( 'paper_colour', '#FAFAFA');?>;
 		}
-		
+
 		.header-text
 		{
 			color: <?php echo of_get_option( 'header-text-colour', '#444444');?>;
@@ -31,12 +31,12 @@
 <body <?php body_class();?> >
 	<nav class="navbar navbar-default navbar-static-top">
 		<div class="container">
-			
+
 			<div class="navbar-header">
 				<button class="navbar-toggle collapsed" data-toggle = "collapse" data-target = ".navHeaderCollapse">
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>	
+					<span class="icon-bar"></span>
 				</button>
 
 				<a href="<?php echo site_url();?>" class="navbar-brand"><?php bloginfo('title'); ?></a>
@@ -53,6 +53,6 @@
                 		'walker'            => new wp_bootstrap_navwalker())
             		);
        			?>
-			</div>		
+			</div>
 		</div>
 	</nav>
