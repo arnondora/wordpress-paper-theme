af14deb28d6cdcc6f92319e38920ac3c752b0776
Arnon Puitrakul <peter_arnon1@icloud.com>
2016-06-12 15:35:46 +0700

    Add some git repository link on the footer

--- PaperTheme/footer.php
+++ PaperTheme/footer.php
@@ -1,7 +1,7 @@
 	<div class = "footer navbar navbar-default" style="margin-bottom:0px;">
 		<div class="container">
 			<p class = "navbar-text pull-left" id = "footer-text">© 2014-<?php echo date('Y') ?> Arnon Puitrakul all right reserved.</p>
-			<p class="navbar-text pull-right" id = "footer-text"> <span class="octicon octicon-code"></span> with <span class="octicon octicon-heart"></span> by <a href="http://www.arnondora.in.th">@arnondora</a>
+			<p class="navbar-text pull-right" id = "footer-text"> <a href = "https://github.com/arnondora/wordpress-paper-theme">PaperTheme</a> is <span class="octicon octicon-code"></span> with <span class="octicon octicon-heart"></span> by <a href="http://www.arnondora.in.th">@arnondora</a>
 		</div>
 	</div>
 <?php wp_footer();?>
