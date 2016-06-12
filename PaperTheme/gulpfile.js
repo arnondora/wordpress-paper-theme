474fa63d653de1acd92bf3f1a7b9a3632de723bf
Arnon Puitrakul <peter_arnon1@icloud.com>
2016-06-13 03:52:45 +0700

    Install gulp-uglify

--- PaperTheme/gulpfile.js
+++ PaperTheme/gulpfile.js
@@ -3,6 +3,7 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
 
 
 
@@ -26,15 +27,14 @@ gulp.task('concatMain', function() {
 });
 
 gulp.task('concatJS', function() {
-  return gulp.src(['./sass/style.scss', './bower_components/bootswatch/bootstrap.css','./bower_components/font-awesome/css/font-awesome.css'])
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(concat('style.css'))
-    .pipe(cleanCSS())
-    .pipe(sourcemaps.write("./css"))
-    .pipe(gulp.dest('./css'));
+  return gulp.src(['./bower_components/jquery/dist/jquery.min.js', './bower_components/bootstrap/js/*.js'])
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write("./js"))
+    .pipe(gulp.dest('./js'));
 });
 
 gulp.task('watch', function(){
-  gulp.watch('./sass/*.scss', ['concatMain','font']);
+  gulp.watch('./sass/*.scss', ['concatMain','font','concatJS']);
   // Other watchers
 });
