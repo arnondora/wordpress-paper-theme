d6125caac751d66df8e20fbff243ec6143611ad1
Arnon Puitrakul <peter_arnon1@icloud.com>
2016-06-12 16:48:27 +0700

    Add head text styling

--- PaperTheme/page.php
+++ PaperTheme/page.php
@@ -5,7 +5,7 @@
 	<div class="container contain-content">
 		<!-- Main Content -->
 		<div class = "shadow-box-page shadow-box-colour" style = "padding-top:5px;">
-			<a href="<?php the_permalink(); ?>"><h3 class= "text-center"><?php the_title(); ?></h3></a>
+			<a href="<?php the_permalink(); ?>"><h3 class= "header-text text-center"><?php the_title(); ?></h3></a>
 			<head><title><?php the_title(); echo ' - '; bloginfo('name'); ?></title></head>
 			<?php if (has_post_thumbnail()) : ?>
 				<div class="row" style = "margin-top:10px;"><center><?php the_post_thumbnail('large',array('class' => 'full-width img-responsive img-thumbnail')); ?></center></div>
