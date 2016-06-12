c577032027884dd60b05cbdaee9469361c52e978
Arnon Puitrakul <peter_arnon1@icloud.com>
2016-06-13 03:28:34 +0700

    Add Social Button on the footer

--- PaperTheme/footer.php
+++ PaperTheme/footer.php
@@ -1,5 +1,13 @@
 	<div class = "footer navbar navbar-default" style="margin-bottom:0px;">
 		<div class="container">
+			<!-- Social Info  -->
+			<div class = "container footer-social-buttons text-center">
+				<a class = "social-btn social-btn-facebook btn fa fa-lg fa-facebook" href="https://www.facebook.com/arnondora"></a>
+				<a class = "social-btn social-btn-twitter btn fa fa-lg fa-twitter" href="https://twitter.com/arnondora"></a>
+				<a class = "social-btn social-btn-google-plus btn fa fa-lg fa-google-plus" href="https://plus.google.com/+ArnonPuitrakul"></a>
+				<a class = "social-btn social-btn-instagram btn fa fa-lg fa-instagram" href="https://www.instagram.com/arnondora/"></a>
+				<a class = "social-btn social-btn-linkedin btn fa fa-lg fa-linkedin" href="https://www.linkedin.com/in/arnondora"></a>
+			</div>
 			<p class = "navbar-text pull-left" id = "footer-text">© 2014-<?php echo date('Y') ?> Arnon Puitrakul all right reserved.</p>
 			<p class="navbar-text pull-right" id = "footer-text"> <a href = "https://github.com/arnondora/wordpress-paper-theme">PaperTheme</a> is <span class="octicon octicon-code"></span> with <span class="octicon octicon-heart"></span> by <a href="http://www.arnondora.in.th">@arnondora</a>
 		</div>
